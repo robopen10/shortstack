b9c30e4264d32e22a0932dc07ca94f43a064ba38
LegitPotato <robopen10@gmail.com>
2021-05-19 16:59:07 -0400

    Initial commit

---
+++ helix-skeleton-master/schema/items/bags/sh_alicepack.lua
@@ -0,0 +1,7 @@
+ITEM.name = "ALICE Pack"
+ITEM.description = "A large backpack."
+ITEM.model = Model("models/props_junk/garbage_bag001a.mdl")
+ITEM.width = 2
+ITEM.height = 2
+ITEM.invWidth = 8
+ITEM.invHeight = 6
